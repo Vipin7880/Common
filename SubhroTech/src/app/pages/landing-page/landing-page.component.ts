87d1090a8488dec5d05173460fef8f9dc808f5ae
Vipin7880 <kushwahavipin313@gmail.com>
2026-07-11 13:09:56 +0530

    feat: scaffold core page components and implement initial UI structure with global styles and assets

--- SubhroTech/src/app/pages/landing-page/landing-page.component.ts
+++ SubhroTech/src/app/pages/landing-page/landing-page.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
+  clientCount: number = 0;
+  partnerCount: number = 0;
+  projectCount: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.clientCount = Math.floor(Math.random() * (200 - 100 + 1)) + 100;
+    this.partnerCount = Math.floor(Math.random() * (50 - 30 + 1)) + 30;
+    this.projectCount = Math.floor(Math.random() * (300 - 200 + 1)) + 200;
   }
 
 }
